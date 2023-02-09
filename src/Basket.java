f54de55bf38e126ad0310b87139dd41758180201
Kapitalist-RF <den.ry97@gmail.com>
2023-02-09 10:53:41 +0300

    basket

---
+++ src/Basket.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Basket {
+
+    private int[] prices;
+    private String[] products;
+    private int[] baskets;
+
+    public Basket(int[] prices, String[] products) {
+        this.prices = prices;
+        this.products = products;
+        this.baskets = new int[products.length];
+    }
+
+    private Basket(int[] prices, String[] products, int[] baskets) {
+        this.prices = prices;
+        this.products = products;
+        this.baskets = baskets;
+    }
+
+    public void addToCart(int productNum, int amount) {
+        if (productNum < products.length && productNum >= 0) {
+            baskets[productNum -1] += amount;
+        }
+    }
+
+    public void printCart() {
+        int sum = 0;
+        StringBuilder sb = new StringBuilder();
+        System.out.println("My Basket: ");
+        if (!Arrays.stream(baskets).allMatch(o1 -> o1 == 0)) {
+            for (int i = 0; i < baskets.length; i++) {
+                sb.delete(0, sb.length());
+                if (baskets[i] != 0) {
+                    sb.append(i + 1)
+                            .append(". ")
+                            .append(products[i])
+                            .append(" - ")
+                            .append(baskets[i])
+                            .append(" шт. ")
+                            .append(prices[i])
+                            .append(" руб/шт ")
+                            .append(baskets[i] * prices[i])
+                            .append(" руб в сумме.");
+                    System.out.println(sb);
+                    sum += baskets[i] * prices[i];
+                }
+            }
+            System.out.println("Итого: " + sum + " руб.");
+        }
+    }
+
+    public void saveTxt(File textFile) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(textFile))) {
+            for (String product : products) {
+                writer.write(product + " ");
+            }
+            writer.newLine();
+            for (int price : prices) {
+                writer.write(price + " ");
+            }
+            writer.newLine();
+            for (int basket : baskets) {
+                writer.write(basket + " ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        if (textFile.exists() && textFile.canRead()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(textFile))) {
+                String str = null;
+                List<String> list = new ArrayList<>();
+                while ((str = reader.readLine()) != null){
+                    list.add(str);
+                }
+                if(list.size() == 3) {
+                    String[] products = list.get(0).split(" ");
+                    int[] prices = Arrays.stream(list.get(1).split(" "))
+                            .flatMapToInt(o1 -> IntStream.of(Integer.parseInt(o1))).toArray();
+                    int[] baskets = Arrays.stream(list.get(2).split(" "))
+                            .flatMapToInt(o1 -> IntStream.of(Integer.parseInt(o1))).toArray();
+                    return new Basket(prices, products, baskets);
+                } else {
+                    return null;
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public int[] getPrices() {
+        return prices;
+    }
+
+    public String[] getProducts() {
+        return products;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("basket.txt");
+        Basket basket = Basket.loadFromTxtFile(file);
+        if (basket == null) {
+            String[] products = {"Хлеб", "Яблоки", "Молоко"};
+            int[] prices = {100, 200, 300};
+            basket = new Basket(prices, products);
+        } else {
+            basket.printCart();
+        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                shoppingList(basket.getProducts(), basket.getPrices());
+                String[] myPursh = reader.readLine().split(" ");
+                if(myPursh[0].equalsIgnoreCase("end")){
+                    break;
+                }
+                basket.addToCart(Integer.parseInt(myPursh[0]), Integer.parseInt(myPursh[1]));
+                basket.saveTxt(file);
+                basket.printCart();
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void shoppingList(String[] products, int[] prices) {
+        System.out.println("Список возможных товаров для покупки: ");
+        for (int i = 0; i < products.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(i + 1)
+                    .append(". ")
+                    .append(products[i])
+                    .append(" ")
+                    .append(prices[i])
+                    .append(" руб/шт");
+            System.out.println(sb.toString());
+        }
+        System.out.println("Выберите товар и количество или введите `end`");
+    }
+
+}
